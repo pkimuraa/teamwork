e6420c5450e2d9c69b1e7d2a29ccfc4c6a5a4b6a
pkimuraa <paulo.kimura94@gmail.com>
2022-07-10 20:11:16 -0300

    Remove initial nuxt comps

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,38 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <ImageBox class="item"/>
+    <div class="item">
+      <Accordion 
+        :tag="data.tag"
+        :title="data.title"
+        :data="data.items"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import Accordion from '../components/accordion.vue';
+
 export default {
-  name: 'IndexPage'
+  components: { 
+    Accordion 
+  },
+  data(){
+    return {
+      data: null,
+    }
+  },  
+  async fetch(){
+    this.data = await this.$http.$get('https://eoyge3duj7xtdqd.m.pipedream.net/');
+  },
+
 }
 </script>
+
+<style>
+.title{
+  padding-left: 18px !important;
+  margin-bottom: 1.125rem;
+}
+</style>
